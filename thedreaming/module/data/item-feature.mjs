89640eebfbb0c68fb2c93c8cee81fb5ae6bba7a6
Befuzin <befuzin@gmail.com>
2025-04-12 20:23:31 +0200

    Delete thedreaming directory

--- thedreaming/module/data/item-feature.mjs
+++
@@ -1,8 +0,0 @@
-import thedreamingItemBase from './base-item.mjs';
-
-export default class thedreamingFeature extends thedreamingItemBase {
-  static LOCALIZATION_PREFIXES = [
-    'THEDREAMING.Item.base',
-    'THEDREAMING.Item.Feature',
-  ];
-}
