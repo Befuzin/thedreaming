88189b027557e32fe20132c14426327de65d80da
Befuzin <befuzin@gmail.com>
2025-04-12 05:39:06 +0200

    Add files via upload

---
+++ thedreaming/module/data/item-feature.mjs
@@ -0,0 +1,8 @@
+import thedreamingItemBase from './base-item.mjs';
+
+export default class thedreamingFeature extends thedreamingItemBase {
+  static LOCALIZATION_PREFIXES = [
+    'THEDREAMING.Item.base',
+    'THEDREAMING.Item.Feature',
+  ];
+}
